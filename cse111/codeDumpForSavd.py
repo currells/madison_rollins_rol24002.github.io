84d19e877768ab1a627dd0f1270f1baf8d71ba89
rol24002 <rol24002@byui.edu>
2025-02-03 11:21:56 -0700

    save

---
+++ cse111/codeDumpForSavd.py
@@ -0,0 +1,8 @@
+""" Creating a program that plays I am a Child of God """
+# enter pip3 install pydub into terminal to install playsound
+
+
+from pydub import AudioSegment
+from pydub.playback import play
+music = AudioSegment.from_mp3("./song.mp3")
+play(music)
