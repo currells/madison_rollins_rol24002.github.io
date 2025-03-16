a65082d23f1afcc0f989cc5f1563ef04a6b20391
Madison Rollins <rol24002@byui.edu>
2025-03-15 22:26:22 -0600

    AHHHHHHH IT WORKED SAFHDUHEWFFAWFWF

--- wdd131/recipies/recipes.js
+++ wdd131/recipies/recipes.js
@@ -1,7 +1,6 @@
 // Import recipies from the .mjs file
-import { recipes } from './recipies.mjs';
+import recipes from './recipes.mjs';
 
-print(recipes);
 
 // create math function ro generate random numb
 function random(num){
@@ -16,8 +15,13 @@ function getRandomRecipie(list){
 }
 
 // function to generate tags HTML
+
 function tagsTemplate(tags) {
-    return tags.map(tag => `<li>${tag}</li>`).join('');
+    if (Array.isArray(tags)) {
+        return tags.map(tag => `<li>${tag}</li>`).join('');
+    } else {
+        return ''; // Return an empty string or some default value
+    }
 }
 
 // function to generate rating HTML
@@ -45,16 +49,18 @@ function recipeTemplate(recipe) {
     return `<figure class="recipe">
     <img src="${recipe.image}" alt="image of ${recipe.name}" />
     <figcaption>
-        <ul class="recipe__tags">
-            ${tagsTemplate(recipe.tags)}
-        </ul>
-        <h2><a href="#">${recipe.name}</a></h2>
-        <p class="recipe__ratings">
-            ${ratingTemplate(recipe.rating)}
-        </p>
-        <p class="recipe__description">
-            ${recipe.description}
-        </p>
+        <div class="recipe-info">
+            <ul class="recipe__tags">
+               ${tagsTemplate(recipe.tags)}
+            </ul>
+            <h2 class="recipe-name"><a href="#">${recipe.name}</a></h2>
+            <p class="rating">
+                ${ratingTemplate(recipe.rating)}
+            </p>
+            <p class="recipe__description">
+                ${recipe.description}
+            </p>
+        </div>
     </figcaption>
     </figure>`;
 }
@@ -78,8 +84,7 @@ function filterRecipes(query) {
         return (
             recipe.name.toLowerCase().includes(lowerCaseQuery) ||
             recipe.description.toLowerCase().includes(lowerCaseQuery) ||
-            recipe.tags.find(tag => tag.toLowerCase().includes(lowerCaseQuery)) ||
-            recipe.recipeIngredient.find(ingredient => ingredient.toLowerCase().includes(lowerCaseQuery))
+            recipe.tags.find(tag => tag.toLowerCase().includes(lowerCaseQuery)) 
         );
     }).sort((a, b) => a.name.localeCompare(b.name));
 }
@@ -88,12 +93,16 @@ function filterRecipes(query) {
 function searchHandler(event) {
     event.preventDefault();
     const query = document.getElementById('search-input').value.toLowerCase();
+    console.log("Search Query:", query);
+
     const filteredRecipes = filterRecipes(query);
+    console.log("Filtered Recipes:", filteredRecipes);
+
     renderRecipes(filteredRecipes);
 }
 
 //event listhererw for search
-document.getElementById('search-form').addEventListener('click', searchHandler);
+document.getElementById('search-form').addEventListener('submit', searchHandler);
 
 function init() {
     // get a random recipe
@@ -105,6 +114,7 @@ function init() {
 init();
 
 
+
 // to test
-const recipe = getRandomListEntry(recipes);
+//const recipe = getRandomListEntry(recipes);
 console.log(recipeTemplate(recipe));
